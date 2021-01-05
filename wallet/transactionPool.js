2b4bbbcc55aacb55acb23d3ca61b8484d552195b
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2021-01-05 18:11:40 +0530

    add function to find existing transaction

--- wallet/transactionPool.js
+++ wallet/transactionPool.js
@@ -5,6 +5,12 @@ class TransactionPool {
 	addTransaction(transaction) {
 		this.transactionsMap[transaction.id] = transaction;
 	}
+	existingTransaction({ inputAddress }) {
+		const transactions = Object.values(this.transactionsMap);
+		return transactions.find(
+			(transaction) => transaction.input.address === inputAddress
+		);
+	}
 }
 
 module.exports = { TransactionPool };
