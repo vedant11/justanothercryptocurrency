a442e870c243483b5379f26cf548ae44945d1d16
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2021-05-01 22:05:39 +0530

    looks good

--- dev.test.sh
+++ dev.test.sh
@@ -1,3 +1,4 @@
-# npm run dev &> logfile & process=$!;
-# tail -F logfile | grep -m1 'app started' && kill $process && echo $process;
-exit 0;
+# process
+# npm run dev | export process=$! & echo "$! works| real is $process" && kill -9 process;
+npm run dev &> logfile & process=$!;
+tail -F logfile | grep -m1 'app started' && kill $process && echo $process && exit 0;
