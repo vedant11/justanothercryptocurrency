8c81a2e73e1d8ca7c638c227a6a63e7281544be7
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2021-05-12 11:56:57 +0530

    add basic walletInfo page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-render(<App />, document.getElementById('root'));
+
+render(
+	<>
+		{/* boostrap css */}
+		<link
+			rel='stylesheet'
+			href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css'
+			integrity='sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l'
+			crossOrigin='anonymous'
+		/>
+		<App />
+	</>,
+	document.getElementById('root')
+);
